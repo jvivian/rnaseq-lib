fa443919c1160d0d255257ad501167010e621da8
John Vivian <jtvivian@gmail.com>
2017-08-12 17:03:58 -0700

    Move to src directory for imports

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rnaseq-lib',
-      version='1.0a1',
+      version='1.0a2',
       description='',
       url='http://github.com/jvivian/rnaseq-lib',
       author='John Vivian',
