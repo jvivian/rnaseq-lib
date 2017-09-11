4c4088a2f4f4701a38c93aee37b72d87251aae2a
John Vivian <jtvivian@gmail.com>
2017-09-11 14:43:00 -0700

    Generalize boxplot to any number of groups

--- src/rnaseq_lib/plotting/__init__.py
+++ src/rnaseq_lib/plotting/__init__.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import holoviews as hv
 
+from rnaseq_lib.utils import flatten
+
 
 def plot_boxplot(df,
-                 samples_a, samples_b,
-                 label_a, label_b,
+                 plot_info,
                  feature,
                  norm_func=None,
                  value_label='counts', group_label='dataset'):
     """
-    Return holoviews boxplot object for a samples by feature DataFrame
+    Return holoviews boxplot object for a "samples by feature" DataFrame
 
     :param pd.DataFrame df: Input DataFrame
-    :param list[str] samples_a: list of samples for group A
-    :param list[str] samples_b: list of samples for group B
-    :param str label_a: Label for group A
-    :param str label_b: Label for group B
+    :param dict(str, list(str)) plot_info: Dict in the form "Label: [Samples]"
     :param str feature: Feature (column label) to use
     :param func norm_func: Normalization function for dataframe
     :param str value_label: Label to use for values in boxplot
@@ -27,7 +25,14 @@ def plot_boxplot(df,
     if norm_func:
         df = df.apply(norm_func)
 
-    plot = {value_label: df.loc[samples_a][feature].tolist() + df.loc[samples_b][feature].tolist(),
-            group_label: [label_a for _ in samples_a] + [label_b for _ in samples_b]}
+    # Define group label
+    group = []
+    for label in sorted(plot_info):
+        group.extend([label for _ in plot_info[label]])
 
+    # Create dictionary with plot info
+    plot = {value_label: flatten([df.loc[plot_info[x]][feature].tolist() for x in sorted(plot_info)]),
+            group_label: group}
+
+    # Return Holoviews BoxWhisker object
     return hv.BoxWhisker(pd.DataFrame.from_dict(plot), kdims=['dataset'], vdims=['counts'])
--- src/rnaseq_lib/utils/tissues.py
+++ src/rnaseq_lib/utils/tissues.py
@@ -120,16 +120,21 @@ def get_normal_samples(tissue):
     return [x for x in samples[tissue] if x.endswith('-11')]
 
 
-def plot_gene_boxplot(df, tissue, gene):
+def gene_expression_boxplot(df, tissue, gene, normal=False):
     """
     Returns a holoviews box and whisker object of a gene for a tissue
 
-    :param pd.DataFrame df:
-    :param str tissue:
-    :param str gene:
+    :param pd.DataFrame df: Gene by Samples DataFrame
+    :param str tissue: Tissue to use
+    :param str gene: Gene to use
+    :param bool normal: If True, will include TCGA normal samples
     :return: Boxplot of gene for tissue
     :rtype: hv.BoxWhisker
     """
-    tumor = get_tumor_samples(tissue)
-    gtex = get_gtex_samples(tissue)
-    return plot_boxplot(df, gtex, tumor, 'GTEx', 'Tumor', gene, norm_func=lambda x: np.log2(x + 1))
+    plot_info = {'GTEx': get_gtex_samples(tissue),
+                 'Tumor': get_tumor_samples(tissue)}
+
+    if normal:
+        plot_info['Normal'] = get_normal_samples(tissue)
+
+    return plot_boxplot(df, plot_info=plot_info, feature=gene, norm_func=lambda x: np.log2(x + 1))
