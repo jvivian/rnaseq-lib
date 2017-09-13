3718ac136317cc0d4e2f3b4501ec4d2b9f7dcdfd
John Vivian <jtvivian@gmail.com>
2017-09-12 17:31:59 -0700

    Moved tissue functions to own directory

--- src/rnaseq_lib/tissues/__init__.py
+++ src/rnaseq_lib/tissues/__init__.py
@@ -1,10 +1,7 @@
 import os
 import pickle
 
-import numpy as np
 import pandas as pd
-
-from rnaseq_lib.plotting import plot_boxplot
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -118,23 +115,3 @@ def get_normal_samples(tissue):
     """
     samples = return_samples()
     return [x for x in samples[tissue] if x.endswith('-11')]
-
-
-def gene_expression_boxplot(df, tissue, gene, normal=False):
-    """
-    Returns a holoviews box and whisker object of a gene for a tissue
-
-    :param pd.DataFrame df: Gene by Samples DataFrame
-    :param str tissue: Tissue to use
-    :param str gene: Gene to use
-    :param bool normal: If True, will include TCGA normal samples
-    :return: Boxplot of gene for tissue
-    :rtype: hv.BoxWhisker
-    """
-    plot_info = {'GTEx': get_gtex_samples(tissue),
-                 'Tumor': get_tumor_samples(tissue)}
-
-    if normal:
-        plot_info['Normal'] = get_normal_samples(tissue)
-
-    return plot_boxplot(df, plot_info=plot_info, feature=gene, title=tissue, norm_func=lambda x: np.log2(x + 1))
