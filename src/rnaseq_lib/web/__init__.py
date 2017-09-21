600286935bff6757bbc6d6d6df9fdcf2d1e7cf19
John Vivian <jtvivian@gmail.com>
2017-09-20 18:10:22 -0700

    Add wikipedia scraping
    Minor formatting

--- src/rnaseq_lib/web/__init__.py
+++ src/rnaseq_lib/web/__init__.py
@@ -24,18 +24,48 @@ def get_drug_target_from_wiki(drug):
     # Parse page
     soup = BeautifulSoup(page, 'html.parser')
 
-    # Look for table
+    # Look for table via HTML tags
     name_box = soup.find('table', {'class': 'infobox'})
+    if name_box:
 
-    if not name_box:
-        print 'No table found for {}'.format(drug)
+        # Look for "Target", next item in the list should be the drug name
+        name = name_box.text.strip()
+        if 'Target' in name:
+            return name.split('\n')[name.split('\n').index('Target') + 1]
+        else:
+            print '{} has no listed Target'.format(drug)
+            return None
 
-    # Look for Target, next item should be the Drug
-    name = name_box.text.strip()
-    if 'Target' in name:
-        return name.split('\n')[name.split('\n').index('Target') + 1]
     else:
-        print '{} has no listed Target'.format(drug)
+        print 'No table found for {}'.format(drug)
+        return None
+
+
+def get_info_from_wiki(drug):
+    """
+    Scrape wikipedia for all information about a drug
+
+    :param str drug: Drug to lookup
+    :return: Information on the wikipedia page
+    :rtype: str
+    """
+    # Look for wiki page
+    url = 'https://en.wikipedia.org/wiki/'
+    try:
+        page = urllib2.urlopen(url + drug)
+    except urllib2.HTTPError:
+        print 'Page not found for: {}'.format(drug)
+        return None
+
+    # Parse page
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # Scrape all content
+    name_box = soup.find('div', {'class': 'mw-content-ltr'})
+    if name_box:
+        return name_box.text.strip()
+    else:
+        print 'No table found for {}'.format(drug)
         return None
 
 
@@ -51,7 +81,6 @@ def get_drug_usage_nih(drug):
     params = {'drug_name': drug}
     url = 'https://dailymed.nlm.nih.gov/dailymed/services/v2/spls.json'
     r = _rget(url, params=params)
-
     if r:
 
         # Get "set ID" to query SPLS for detailed info
@@ -60,13 +89,11 @@ def get_drug_usage_nih(drug):
             if drug in data['title'] or drug.upper() in data['title']:
                 print 'Found set ID for: {}'.format(data['title'])
                 setid = data['setid']
-
         if setid:
 
             # Make request
             url = 'https://dailymed.nlm.nih.gov/dailymed/services/v2/spls/{}.xml'.format(setid)
             r = _rget(url)
-
             if r:
 
                 # I hate XML with all my being
@@ -86,30 +113,20 @@ def get_drug_usage_nih(drug):
                     for item in remove:
                         content = content.replace(item, '')
                     return content.replace('displayName', '\n\n')
+
                 else:
                     print 'No content found'
                     return None
+
             else:
                 return None
+
         else:
             print 'No set ID found for {}'.format(drug)
             return None
+
     else:
         return None
-
-
-def grep_cancer_terms(content):
-    """
-    Returns sentences with cancer terms
-
-    :param str content: String containing sentences to check for cancer terms
-    :return: Sentences with matches
-    :rtype: list(str)
-    """
-    terms = {'cancer', 'leukemia', 'carcinoma', 'squamous', 'lymphoma',
-             'malignant', 'metastasis', 'metastatic', 'sarcoma', 'tumor'}
-
-    return [x.upper() for x in content.split('.') if any(y.upper() for y in terms if y in x)]
 
 
 def _rget(url, params=None):
