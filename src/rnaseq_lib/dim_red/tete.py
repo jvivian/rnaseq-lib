8c2bf436f20f4ed299067b4e665ebfa4ac71eaea
John Vivian <jtvivian@gmail.com>
2017-10-25 21:09:21 -0700

    Update tete algorithm

--- src/rnaseq_lib/dim_red/tete.py
+++ src/rnaseq_lib/dim_red/tete.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 """
 Created on Sat May 27 12:46:25 2017
@@ -12,31 +13,36 @@ Created on Fri May 26 12:17:14 2017
 @author: ehsanamid
 """
 
+import sys
 from sklearn.neighbors import NearestNeighbors as knn
 #from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
 import numpy as np
-import pylab as plot
+import time
     
-def generate_triplets(X, k=20):
-    num_extra = np.maximum(k+10, 30) # look up more neighbors
+def generate_triplets(X, kin=50, weight_adj=False, random_triplets=True):
+    num_extra = np.maximum(kin+50, 50) # look up more neighbors
+#    num_extra = kin
+    kout = 10
     n = X.shape[0]
     nbrs = knn(n_neighbors= num_extra + 1, algorithm='auto').fit(X)
     distances, indices = nbrs.kneighbors(X)
-    #    sig = distances[:,10]
-    sig = np.mean(distances[:, 10:15], axis=1) # scale parameter
+#    sig = distances[:,10]
+    sig = np.maximum(np.mean(distances[:, 10:20], axis=1), 1e-20) # scale parameter
     P = np.exp(-distances**2/np.reshape(sig[indices.flatten()],[n, num_extra + 1])/sig[:,np.newaxis])
     sort_indices = np.argsort(-P, axis = 1) # actual neighbors
-    triplets = np.zeros([n * k**2, 3])
-    weights = np.zeros(n * k**2)
+    
+    triplets = np.zeros([n * kin * kout, 3])
+    weights = np.zeros(n * kin * kout)
+    
     cnt = 0
     for i in xrange(n):
-        for j in xrange(k):
+        for j in xrange(kin):
             sim = indices[i,sort_indices[i,j+1]]
             p_sim = P[i,sort_indices[i,j+1]]
             rem = indices[i,sort_indices[i,:j+2]].tolist()
             l = 0
-            while (l < k):
+            while (l < kout):
                 out = np.random.choice(n)
                 if out not in rem:
                     triplets[cnt,:] = [i, sim, out]
@@ -47,13 +53,44 @@ def generate_triplets(X, k=20):
                     rem.append(out)
                     l += 1
                     cnt += 1
-        if ((i+1) % 500) == 0:
-            pass
-            #print 'Genareted triplets %d out of %d' % (i+1, n)
+        if ((i+1) % 10000) == 0:
+            print 'Genareted triplets %d out of %d' % (i+1, n)
+    if random_triplets:
+        kr = 5
+        triplets_rand = np.zeros([n * kr, 3])
+        weights_rand = np.zeros(n * kr)
+        for i in xrange(n):
+            cnt = 0
+            while cnt < kr:
+                sim = np.random.choice(n)
+                if sim == i:
+                    continue
+                out = np.random.choice(n)
+                if out == i or out == sim:
+                    continue
+                p_sim = np.exp(-np.sum((X[i,:]-X[sim,:])**2)/(sig[i] * sig[sim]))
+                if p_sim < 1e-20:
+                    p_sim = 1e-20
+                p_out = np.exp(-np.sum((X[i,:]-X[out,:])**2)/(sig[i] * sig[out]))
+                if p_out < 1e-20:
+                    p_out = 1e-20
+                if p_sim < p_out:
+                    sim, out = out, sim
+                    p_sim, p_out = p_out, p_sim
+                triplets_rand[i * kr + cnt,:] = [i, sim, out]
+                weights_rand[i * kr + cnt] = p_sim/p_out
+                cnt += 1
+            if ((i+1) % 10000) == 0:
+                print 'Genareted random triplets %d out of %d' % (i+1, n)
+        triplets = np.vstack((triplets, triplets_rand))
+        weights = np.hstack((weights, weights_rand))
+    triplets = triplets[~np.isnan(weights),:]
+    weights = weights[~np.isnan(weights)]
     weights /= np.max(weights)
-    weights += 0.01
-    weights = np.log(1 + 50 * weights)
-    weights /= np.max(weights)
+    weights += 0.001
+    if weight_adj:
+        weights = np.log(1 + 50 * weights)
+        weights /= np.max(weights)
     return (triplets.astype(int), weights.flatten())
         
 def tete_grad(Y, triplets, weights):
@@ -75,7 +112,7 @@ def tete_grad(Y, triplets, weights):
     return (loss, grad, num_viol)
     
 
-def tete(X, num_dims = 2, num_neighbs = 20):
+def tete(X, num_dims = 2, num_neighbs = 50, Yinit=[]):
     n, dim = X.shape
     X -= np.min(X)
     X /= np.max(X)
@@ -85,7 +122,10 @@ def tete(X, num_dims = 2, num_neighbs = 20):
 #        pca.fit(X)
 #        X = np.dot(X, pca.components_.transpose())
         X = TruncatedSVD(n_components=50, random_state=0).fit_transform(X)
-    Y = np.random.normal(size=[n, 2]) * 0.0001
+    if np.size(Yinit) > 0:
+        Y = Yinit
+    else:
+        Y = np.random.normal(size=[n, 2]) * 0.0001
     C = np.inf
     best_C = np.inf
     best_Y = Y
@@ -100,7 +140,7 @@ def tete(X, num_dims = 2, num_neighbs = 20):
         old_C = C
         C, grad, num_viol = tete_grad(Y, triplets, weights)
         
-        # maintain best answer
+        # maintain the best answer
         if C < best_C:
             best_C = C
             best_Y = Y
@@ -115,17 +155,35 @@ def tete(X, num_dims = 2, num_neighbs = 20):
             eta = eta * 0.5
         
         if (itr+1) % 100 == 0:
-            pass
-            # print 'Iteration: %4d, Loss: %3.3f, Num viol: %0.4f' % (itr+1, C, float(num_viol)/num_triplets)
-    #np.savetxt('result_tete_outlier_10000.txt',best_Y)
+            print 'Iteration: %4d, Loss: %3.3f, Violated triplets: %0.4f' % (itr+1, C, float(num_viol)/num_triplets)
     return best_Y
 
+def load_known_size(fname):
+    t = time.time()
+    first = True
+    with open(fname) as f:
+        for irow, line in enumerate(f):
+            if first:
+                nrow, ncol = [int(a) for a in line.split()]
+                x = np.empty((nrow, ncol), dtype = np.double)
+                first = False
+            else:
+                x[irow-1, :] = [float(a) for a in line.split()]
+    print "Elapsed time %s" % (time.time() - t)
+    return x
 
-if __name__ == "__main__":
-    print "Run Y = tete.tete(X, num_dims, num_neighbs) to perform t-ETE on your dataset."
-    X = np.loadtxt("mnist2500_X.txt")
-    labels = np.loadtxt("mnist2500_labels.txt")
-    Y = tete(X, 2, 20)
-    plot.scatter(Y[:,0], Y[:,1], 10, labels)
-    plot.show();
-
+def main():
+    filename = sys.argv[1]
+    X = load_known_size(filename)
+    if len(sys.argv) > 2:
+        init = sys.argv[2]
+        Yinit = np.loadtxt(init)
+        Y = tete(X, 2, 50, Yinit)
+    else:
+        Yinit = np.random.normal(size=[X.shape[0], 2]) * 0.0001
+        Y = tete(X, 2, 50, Yinit)
+        # np.savetxt('../data/' + filename[filename.find('/',3)+1:filename.find('.',3)] + '_init_sb.txt',Yinit)
+    np.savetxt('../results/' + filename[filename.find('/',3)+1:filename.find('.',3)] + '_2D_sb.txt',Y)
+    
+if __name__ == '__main__':
+    main()
