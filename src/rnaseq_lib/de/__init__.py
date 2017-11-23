660b150313101f6e8c18b105024c915026d0ce87
John Vivian <jtvivian@gmail.com>
2017-11-22 22:13:23 -0800

    Add l2fc docstring

--- src/rnaseq_lib/de/__init__.py
+++ src/rnaseq_lib/de/__init__.py
@@ -1,4 +1,13 @@
 import numpy as np
 
-def get_l2fc(a, b):
-    return np.log2(b + 1) - np.log2(a + 1)
+def l2fc(a, b):
+    """
+    Calculate the log2 Fold Change between two arrays, floats, or integers
+    a and b cannot be, nor contain, values less than 0
+
+    :param (int/float/np.array) a: Value or array
+    :param (int/float/np.array) b: Value or array
+    :return: L2FC array or value
+    :rtype: (int/float/np.array)
+    """
+    return np.log2(a + 1) - np.log2(b + 1)
