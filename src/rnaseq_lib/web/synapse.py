3b2fae7875d89adb8537b75c7e9b48a8663a9d4f
John Vivian <jtvivian@gmail.com>
2017-11-22 22:13:54 -0800

    Add download and login functions

--- src/rnaseq_lib/web/synapse.py
+++ src/rnaseq_lib/web/synapse.py
@@ -15,12 +15,34 @@ def upload_file(file_path, login, parent, description=None):
     :param str parent: Parent Synapse ID (example: syn12312415) where file will be placed
     :param str description: Optional description to add
     """
-
     description = '' if None else description
     f = File(file_path, description=description, parent=parent)
 
-    assert 'SYNAPSE_PASS' in os.environ, 'SYNAPSE_PASS must be set as an environment variable'
+    syn = _syn_login(login)
+    syn.store(f)
 
+
+def download_file(synid, login, download_location='.'):
+    """
+    Synapse ID of file to download
+
+    :param str synid: Synapse ID
+    :param str login: Synapse ID
+    :param str download_location: Download location for file
+    """
+    syn = _syn_login(login)
+    syn.get(synid, downloadLocation=download_location)
+
+
+def _syn_login(login):
+    """
+    Login to synapse. Set environment variable SYNAPSE_PASS to the password for `login`
+
+    :param str login:
+    :return: Synapse instance
+    :rtype: instance
+    """
+    assert 'SYNAPSE_PASS' in os.environ, 'SYNAPSE_PASS must be set as an environment variable'
     syn = Synapse()
     syn.login(login, os.environ['SYNAPSE_PASS'])
-    syn.store(f)
+    return syn
