1a83696454d5be09b07d1e1e6a23ea76c77012a9
John Vivian <jtvivian@gmail.com>
2017-11-28 12:23:19 -0800

    Fix global imports

--- src/rnaseq_lib/__init__.py
+++ src/rnaseq_lib/__init__.py
@@ -1,15 +1,19 @@
-import rnaseq_lib.R
 import rnaseq_lib.civic
 import rnaseq_lib.data
-import rnaseq_lib.de
+import rnaseq_lib.diff_exp
 import rnaseq_lib.dim_red
 import rnaseq_lib.docker
 import rnaseq_lib.drugs
 import rnaseq_lib.graphs
 import rnaseq_lib.gtf
 import rnaseq_lib.images
-import rnaseq_lib.plotting
+
+import rnaseq_lib.plot
+import rnaseq_lib.plot.dr
+import rnaseq_lib.plot.hview
 import rnaseq_lib.tissues
+import rnaseq_lib.tissues.plots
+
 import rnaseq_lib.utils
 
 import rnaseq_lib.web
