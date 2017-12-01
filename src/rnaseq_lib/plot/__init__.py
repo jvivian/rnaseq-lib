5ed0bb305a13252d05887f929f23b949cffe1c31
John Vivian <jtvivian@gmail.com>
2017-12-01 12:06:41 -0800

    Refactor Holoview object to __init__

--- src/rnaseq_lib/plot/__init__.py
+++ src/rnaseq_lib/plot/__init__.py
@@ -1,8 +1,127 @@
 import holoviews as hv
 import numpy as np
 import pandas as pd
+
+from rnaseq_lib.diff_exp import log2fc
 from rnaseq_lib.dim_red import run_tsne, run_tete
+from rnaseq_lib.tissues import subset_by_dataset
 from rnaseq_lib.utils import flatten
+
+
+class Holoview:
+    """
+    Object for Holoviews plots. Created for use with Holomap and DynamicMap which cannot
+    accept dataframes as arguments. This class circumvents that limitation by referencing
+    the dataframe internally.
+    """
+
+    def __init__(self, df):
+        """
+        :param pd.DataFrame df: Dataframe containing metadata / expression values (Synapse.org: syn11515015)
+        """
+        self.df = df
+        self.df_cols = ['id', 'tissue', 'dataset', 'tumor', 'type']
+
+    def subset(self, tissue, gene):
+        df = self.df[self.df_cols + [gene]].sort_values(gene, ascending=False)
+        return df[df.tissue == tissue]
+
+    def gene_kde(self, tissue, gene):
+        """
+        Returns KDE of gene expression (log2) for given tissue
+
+        :param str tissue: Tissue (ex: Breast) to select
+        :param str gene: Gene (ex: ERBB2) to select
+        :return: Returns holoviews Overlay object of gene KDE
+        :rtype: hv.Overlay
+        """
+        # Subset dataframe by tissue and gene
+        df = self.subset(tissue, gene)
+
+        # Subset by dataset
+        tumor, normal, gtex = subset_by_dataset(df)
+
+        # Create KDE objects
+        t = hv.Dimension(tumor[gene].apply(lambda x: np.log2(x + 1)), label='Tumor')
+        g = hv.Dimension(gtex[gene].apply(lambda x: np.log2(x + 1)), label='GTEx')
+
+        return t * g
+
+    def gene_curves(self, tissue, gene):
+        """
+        Returns set of 3 plots for tissue / gene given a dataframe of metadata and expression values
+
+        :param str tissue: Tissue (ex: Breast) to select
+        :param str gene: Gene (ex: ERBB2) to select
+        :return: Returns holoviews Layout object containing 3 plots for selected Tisssue / Gene
+        :rtype: hv.Layout
+        """
+        # Subset dataframe for gene and tissue
+        df = self.subset(tissue, gene)
+
+        # Logscale gene for calculations
+        df[gene] = df[gene].apply(lambda x: np.log2(x + 1))
+
+        # Subset by dataset
+        tumor, normal, gtex = subset_by_dataset(df)
+
+        # Get values for plot
+        records = []
+        for perc_tumor in [x * 0.1 for x in xrange(1, 11)]:
+            # Get log2 expression value for top x% tumor samples
+            exp = float(tumor.iloc[int(len(tumor) * perc_tumor) - 1][gene])
+
+            # Get percentage of samples in GTEx
+            perc_normal = (len(gtex[gtex[gene] > exp]) * 1.0) / len(gtex)
+
+            # Compute L2FC for tumor sample subset vs GTEx
+            tumor_mean = tumor.iloc[:int(len(tumor) * perc_tumor) - 1][gene].apply(lambda x: 2 ** x - 1).median()
+            gtex_mean = gtex[gene].apply(lambda x: 2 ** x - 1).median()
+            l2fc = log2fc(tumor_mean, gtex_mean)
+
+            # Store
+            records.append((tissue, exp, l2fc, perc_tumor, perc_normal, len(gtex), len(tumor), 'GTEx'))
+
+        # Create dataframe from records
+        info = pd.DataFrame.from_records(records, columns=['tissue', 'expression',
+                                                           'l2fc',
+                                                           'percent_tumor',
+                                                           'percent_normal',
+                                                           'num_normals', 'num_tumors',
+                                                           'normal_dataset'])
+
+        # Define dimensions
+        tissue_dim = hv.Dimension('tissue', label='Tissue')
+        ptumor_dim = hv.Dimension('percent_tumor', label='% Tumor')
+        pnormal_dim = hv.Dimension('percent_normal', label='percent')
+        l2fc_dim = hv.Dimension('l2fc', label='log2FC')
+        exp_dim = hv.Dimension('expression', label='log2(x+1)')
+
+        # First plot - Percentage of Normal Samples
+        c1 = hv.Curve(data=info, kdims=[ptumor_dim],
+                      vdims=[pnormal_dim, tissue_dim], group='Percentage of Normal Samples',
+                      extents=(None, 0, None, 1))
+
+        s1 = hv.Scatter(data=info, kdims=[ptumor_dim],
+                        vdims=[pnormal_dim, tissue_dim], group='Percentage of Normal Samples')
+
+        # Second Plot - Expression
+        c2 = hv.Curve(data=info, kdims=[ptumor_dim],
+                      vdims=[exp_dim, tissue_dim], group='Gene Expression',
+                      extents=(None, 0, None, 16))
+
+        s2 = hv.Scatter(data=info, kdims=[ptumor_dim],
+                        vdims=[exp_dim, tissue_dim], group='Gene Expression')
+
+        # Third Plot - Log2 Fold Change
+        c3 = hv.Curve(data=info, kdims=[ptumor_dim],
+                      vdims=[l2fc_dim, tissue_dim], group='Log2 Fold Change',
+                      extents=(None, -0.5, None, 8))
+
+        s3 = hv.Scatter(data=info, kdims=[ptumor_dim],
+                        vdims=[l2fc_dim, tissue_dim], group='Log2 Fold Change')
+
+        return (c1 * s1 + c2 * s2 + c3 * s3).cols(1)
 
 
 def plot_boxplot(df,
@@ -91,16 +210,3 @@ def _scatter_dataset(z, title, info=None):
                       kdims=['x'],
                       vdims=['y'] + [x for x in info.keys() if not x == 'x' and not x == 'y'],
                       group=title)
-
-
-def plot_deseq2(df, info):
-    z = np.array(df[['baseMean', 'log2FoldChange']])
-    z[:, 0] = map(lambda x: np.log2(x + 1), z[:, 0])
-    info['x'] = z[:, 0]
-    info['y'] = z[:, 1]
-    info['Name'] = df.index
-
-    return hv.Scatter(pd.DataFrame.from_dict(info),
-                      kdims=[('x', 'Log2 Mean')],
-                      vdims=[('y', 'Log2 Fold Change')] + [k for k in info.keys() if k not in ['x', 'y']],
-                      group='Lung')
