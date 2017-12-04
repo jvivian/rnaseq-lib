bc0c36fcee437649d39ca414677cd6aff5bf38af
John Vivian <jtvivian@gmail.com>
2017-12-04 13:59:55 -0800

    Bump release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rnaseq-lib',
-      version='1.0a23',
+      version='1.0a24',
       description='Library of convenience functions related to current research',
       url='http://github.com/jvivian/rnaseq-lib',
       author='John Vivian',
