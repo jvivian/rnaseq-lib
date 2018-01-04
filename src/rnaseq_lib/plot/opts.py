68cfba3465bfc625fd62aece349429ddde23c892
John Vivian <jtvivian@gmail.com>
2018-01-04 13:23:53 -0800

    Cleanup opts

--- src/rnaseq_lib/plot/opts.py
+++ src/rnaseq_lib/plot/opts.py
@@ -11,16 +11,10 @@ gene_curves_opts = {
                                'style': dict(color='Purple')},
     'Scatter': {'style': dict(color='red', size=3)}}
 
-gene_kde_opts = {
-    'Distribution': {'plot': dict(filled=False),
-                    'style': dict(line_color=hv.Cycle())}
-}
+gene_kde_opts = {'Overlay': {'plot': dict(width=500, legend_position='left')}}
 
-gene_distribution_opts = {
-    'BoxWhisker': {'plot': dict(width=900, xrotation=45)}
-}
+gene_distribution_opts = {'BoxWhisker': {'plot': dict(width=900, xrotation=45)}}
 
 gene_de_opts = {
     'Scatter': {'plot': dict(color_index='Tissue', legend_position='left', width=700, height=500, tools=['hover']),
-                'style': dict(cmap='tab20', size=10, alpha=0.5)}
-}
+                'style': dict(cmap='tab20', size=10, alpha=0.5)}}
