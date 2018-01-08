a3168df16b57869d0f153ab20e253f5d7111ae4a
John Vivian <jtvivian@gmail.com>
2018-01-08 11:53:30 -0800

    Fix HeatMap object spelling

--- src/rnaseq_lib/plot/opts.py
+++ src/rnaseq_lib/plot/opts.py
@@ -25,7 +25,7 @@ gene_de_opts = {
                 'style': dict(cmap='tab20', size=10, alpha=0.5)}}
 
 gene_de_heatmap_opts = {
-    'Heatmap': {'plot': dict(colorbar=True)}
+    'HeatMap': {'plot': dict(colorbar=True)}
 }
 
 sample_count_opts = {
