82916c30eb71085d55d376dbb482840a8e3f7a57
John Vivian <jtvivian@gmail.com>
2018-04-12 22:58:53 -0600

    Remove tumor label

--- src/rnaseq_lib/plot/__init__.py
+++ src/rnaseq_lib/plot/__init__.py
@@ -23,7 +23,7 @@ class Holoview:
         :param pd.DataFrame df: Dataframe containing metadata / expression values (Synapse.org: syn11515015)
         """
         self.df = df
-        self.df_cols = ['tissue', 'tumor', 'type', 'label']
+        self.df_cols = ['id', 'tissue', 'type', 'label']
 
         # Style attributes - used in conjunction with '.opts()'
         self._gene_curves_opts = gene_curves_opts
